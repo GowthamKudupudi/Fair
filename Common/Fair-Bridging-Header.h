c5c159d275a7b621354a3a198b2eef24569b2ee1
Gowtham Kudupudi <gowtham@ferryfair.com>
2018-04-14 09:30:50 +0530

    Add all

---
+++ Common/Fair-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "Matrix4.h"
+//#import <OpenGLES/ES2/glext.h>
+//#import "Icosohedron.h"
+void ExtNormal(const GLfloat* gfSolid, unsigned int uiSolidSize, GLfloat* gfSphere, unsigned int uiSphereSize, unsigned short iRegression, unsigned short usDimensions);
+struct MyFloat3{
+    float x;
+    float y;
+    float z;
+};
+struct MyFloat3 ExtCurl(const float (* const triangle)[][3]);
+//static float* icosoHedronVertices = IcosoHedron::vertices;
+//static float* icosoHedronTrinagles = IcosoHedron::triangles;
---
+++ Fair-macOS/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HelloMetal-macOS
+//
+//  Created by Gowtham Kudupudi on 01/07/17.
+//  Copyright © 2017 Gowtham Kudupudi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
